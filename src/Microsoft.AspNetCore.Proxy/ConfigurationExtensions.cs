47e4ac81d2c5a10a9e233b8ff3aa3b5ffa1e5323
Tommy Long <pete_201@hotmail.com>
2017-12-13 19:16:07 +0000

    added common configuration usage

---
+++ src/Microsoft.AspNetCore.Proxy/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Proxy;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+	    public static ProxyOptions GetProxyOptions(this IConfigurationSection config)
+	    {
+		    // Host & AppendQuery properties will not bind correctly 
+		    // due to objects being ctor sealed 
+		    var proxyOptions = config.Get<ProxyOptions>();
+		    proxyOptions.Host = HostString.FromUriComponent(config["Host"]);
+		    proxyOptions.AppendQuery = QueryString.FromUriComponent(config["AppendQuery"]);
+		    return proxyOptions;
+	    }
+    }
+}
